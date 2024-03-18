4ada67bf0bf338d989ced03807b1b8d93f1f3a3c
Yakov-Varnaev <yakov.varnaev@gmail.com>
2024-03-18 16:35:37 +0300

    cleanup

--- client/api/notifications.js
+++ client/api/notifications.js
@@ -7,28 +7,15 @@ export const connect = () => {
     `ws://${host}/api/notifications/ws`,
   );
 
-  const alertStore = useAlert();
-
-  socket.onopen = (event) => {
-    console.log("WebSocket connection opened:", event);
-  };
-
   socket.onmessage = async (event) => {
     let notification = JSON.parse(event.data);
     const user_id = useAuth().user.id;
-    console.log(user_id);
-    console.log("Update accounts");
     await useAccounts().getAccounts(user_id);
-    console.log("Update transactions");
     await useTransactions().getTransactions();
-    alertStore.reportInfo(notification.message);
-  };
-
-  socket.onclose = (event) => {
-    console.log("WebSocket connection closed:", event);
+    useAlert().reportInfo(notification.message);
   };
 
   socket.onerror = (event) => {
-    console.error("WebSocket error:", event);
+    useAlert().reportError("Oops. Notifications are broken :/.");
   };
 };
